d37adf54b768363b45d439a4b2db301c3be44cbc
Fran Port <franport@outlook.com>
2022-08-03 06:50:46 -0300

    Fixed Logo Position, DarkMode
    DarkMode now stored in localStorage

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,22 +1,28 @@
-function toggleDarkMode(darkModeOn) {
-  if (darkModeOn) {
-    document.documentElement.classList.remove('dark');
-    background.src = 'img/light-mode.jpg';
-    icon.src = 'img/fran_logo_light.png';
-  } else {
-    document.documentElement.classList.add('dark');
-    background.src = 'img/dark-mode.jpg';
-    icon.src = 'img/fran_logo_dark.png';
-  }
-}
-
 const darkModeButton = document.querySelector('#toggleDarkMode');
 let background = document.querySelector('#hero');
 let icon = document.querySelector('#logo');
 
-let darkModeEnabled = true;
+function setDarkMode(darkModeValue) {
+  if (darkModeValue) {
+    localStorage.darkMode = 'enabled';
+    document.documentElement.classList.add('dark');
+    background.src = 'img/dark-mode.jpg';
+    icon.src = 'img/fran_logo_dark.png';
+    darkModeButton.textContent = '☀️';
+  } else {
+    localStorage.darkMode = 'disabled';
+    document.documentElement.classList.remove('dark');
+    background.src = 'img/light-mode.jpg';
+    icon.src = 'img/fran_logo_light.png';
+    darkModeButton.textContent = '🌙';
+  }
+}
+
+let darkModeEnabled;
+localStorage.darkMode === 'enabled' ? (darkModeEnabled = true) : (darkModeEnabled = false);
+setDarkMode(darkModeEnabled);
 
 darkModeButton.addEventListener('click', () => {
-  toggleDarkMode(darkModeEnabled);
   darkModeEnabled = !darkModeEnabled;
+  setDarkMode(darkModeEnabled);
 });
