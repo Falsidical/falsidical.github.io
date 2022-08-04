70e61c07e6d28ec8f03dfa97296bae786ed84b66
Fran Port <franport@outlook.com>
2022-08-03 22:08:17 -0300

    Project update

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,21 +1,24 @@
 const darkModeButton = document.querySelector('#toggleDarkMode');
 let background = document.querySelector('#hero');
 let icon = document.querySelector('#logo');
+let project1Image = document.querySelector('#project1');
 
 function setDarkMode(darkModeValue) {
+  let mode;
   if (darkModeValue) {
     localStorage.darkMode = 'enabled';
+    mode = 'dark';
     document.documentElement.classList.add('dark');
-    background.src = 'img/dark-mode.jpg';
-    icon.src = 'img/fran_logo_dark.png';
     darkModeButton.textContent = '☀️';
   } else {
     localStorage.darkMode = 'disabled';
+    mode = 'light';
     document.documentElement.classList.remove('dark');
-    background.src = 'img/light-mode.jpg';
-    icon.src = 'img/fran_logo_light.png';
     darkModeButton.textContent = '🌙';
   }
+  background.src = `img/${mode}-mode.jpg`;
+  icon.src = `img/fran_logo_${mode}.png`;
+  project1Image.src = `img/project-1-${mode}.png`;
 }
 
 let darkModeEnabled;
